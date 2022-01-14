9ea5c4f180d9991637cd4f84704689dca04450cd
Tim Eisler <TimEisler@outlook.com>
2022-01-13 20:03:23 -0600

    Added missing parens on calls to __next__

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -28,9 +28,9 @@ class BudgetList:
     
     def __next__(self):
         try:
-            return self.iter_e.__next__
+            return self.iter_e.__next__()
         except StopIteration as stop:
-            return self.iter_o.__next__
+            return self.iter_o.__next__()
 
 def main():
     myBudgetList = BudgetList(1200)
